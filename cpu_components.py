19f872271cf0219ce2e2ab62a0c384231a0c09f5
dlculver <dominic.culver@gmail.com>
2022-10-26 13:31:43 +0200

    added cpu_components.py file

---
+++ cpu_components.py
@@ -0,0 +1,2 @@
+## CS104 final project portfolio
+## cpu components file. Currently, will have class for the CPU, later will incoporate an ALU and immediate register classes to more fully simulate CPU and incorporate machine code level stuff. Right now will only incorporate Assembly level langauge
