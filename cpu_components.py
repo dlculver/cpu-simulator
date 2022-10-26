995a9285abb973eb34c0aff9d8bc6c70de15ee21
dlculver <dominic.culver@gmail.com>
2022-10-26 15:02:00 +0200

    started the CPU class, updated memory.py

--- cpu_components.py
+++ cpu_components.py
@@ -1,2 +1,40 @@
 ## CS104 final project portfolio
 ## cpu components file. Currently, will have class for the CPU, later will incoporate an ALU and immediate register classes to more fully simulate CPU and incorporate machine code level stuff. Right now will only incorporate Assembly level langauge
+
+from memory import MainMemory
+
+MEMORY_BUS_SIZE = 128
+MAIN_MEMORY_SIZE = 128
+CACHE_SIZE = 16
+NUMBER_OF_REGISTERS = 9
+
+class CPU:
+    def __init__(self, main_memory_size, cache_size, num_of_registers):
+        self.cpu_counter = 0 ## the counter keeps track of where we are in reading instructions
+        self.registers = [0]*num_of_registers
+        self.memory = MainMemory(main_memory_size)
+        self.main_memory_size = main_memory_size
+        self.num_of_registers = num_of_registers
+
+    ## cpu counter methods
+
+    def increment_cpu_counter(self):
+        self.cpu_counter += 1
+
+    def reset_cpu_counter(self):
+        self.cpu_counter = 0
+
+    def set_cpu_counter(self, value):
+        self.cpu_counter = value
+
+    ## register methods
+
+    def reset_registers(self):
+        self.registers = [0]*self.num_of_registers
+
+    ## methods for interacting with cache
+
+    ## methods for interacting with main memory
+
+    ## assembly language methods
+
--- memory.py
+++ memory.py
@@ -4,17 +4,21 @@
 ## addresses will be written in binary
 
 
-MEMORY_BUS_SIZE = 128
-MAIN_MEMORY_SIZE = 128
-CACHE_SIZE = 16
-
 
 ## main memory class
 class MainMemory:
     def __init__(self, size):
         self.size = size
-        self.initialize_main_memory()
         self.main_memory = {}
+        self.initialize_main_memory()
+
+
+    def __str__(self):
+        string = ''
+        for key, value in self.main_memory.items():
+            string += f'({key}, {str(value)})\n'
+        return string
+
 
     def initialize_main_memory(self):
         for i in range(self.size):
@@ -31,12 +35,13 @@ class MainMemory:
         if type(address) == int:
             address = '{0:08b}'.format(address)
         print(f'CPU writing to main memory at address {address}: {data}')
+        self.main_memory[address] = data
 
 ## adding idx associated to cache for implementing FIFO or other replacement methods, might change later
-class CacheMemory:
-    def __init__(self, size, idx):
-        self.size = size
-        self.idx = idx
-        self.memory = [{'tag':None, 'data': None}] * self.size
-
-    def retrieve_from_cache(self, idx):## finish tomorrow
+# class CacheMemory:
+#     def __init__(self, size, idx):
+#         self.size = size
+#         self.idx = idx
+#         self.memory = [{'tag':None, 'data': None}] * self.size
+#
+#     def retrieve_from_cache(self, idx):## finish tomorrow
